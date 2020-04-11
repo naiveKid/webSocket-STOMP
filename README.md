aaaab72c880c822371d462a8f347e9eff4d31fe9
wolf master <1066394274@qq.com>
2020-04-11 16:38:10 +0800

    源码上传

---
+++ README.md
@@ -0,0 +1,29 @@
+# SpringWebSocket
+​	Spring 4.0为WebSocket通信提供了支持，包括： 
+
+- ​	发送和接收消息的低层级API；
+- ​	发送和接收消息的高级API；
+- ​	用来发送消息的模板；
+- ​	支持SockJS，用来解决浏览器端、服务器以及代理不支持WebSocket的问题。 
+
+
+
+### spring websocket可选择以下几种方式
+
+​	**使用Spring的低层级WebSocket API：**
+
+​		直接使用WebSocket（或SockJS）就很类似于使用TCP套接字来编写Web应用。因为没有高层级的线路协议（wire protocol），因此就需要我 们定义应用之间所发送消息的语义，还需要确保连接的两端都能遵循这些语义。 不过，好消息是我们并非必须要使用原生的WebSocket连接。就像HTTP在TCP套接字之上添加了请求-响应模型层一样，STOMP在 WebSocket之上提供了一个基于帧的线路格式（frame-based wire format）层，用来定义消息的语义。 
+
+
+
+​	**使用STOMP简单代理：**
+
+​	在Spring MVC中为控制器方法添加@MessageMapping注解，使其处理STOMP消息，它与带 有@RequestMapping注解的方法处理HTTP请求的方式非常类似。 
+
+
+
+​	**使用STOMP代理中继:(RabbitMQ、 ActiveMQ )**
+
+ 	对于初学来讲，简单的代理是很不错的，但是它也有一些限制。尽管它模拟了STOMP消息代理，但是它只支持STOMP命令的子集。因为它是 基于内存的，所以它并不适合集群，因为如果集群的话，每个节点也只能管理自己的代理和自己的那部分消息。 
+
+​	对于生产环境下的应用来说，你可能会希望使用真正支持STOMP的代理来支撑WebSocket消息，如RabbitMQ或ActiveMQ。这样的代理提供了 可扩展性和健壮性更好的消息功能，当然它们也会完整支持STOMP命令。  
---
+++ src/main/java/org/springframework/websocket/config/WebSocketStompConfig.java
@@ -0,0 +1,109 @@
+package org.springframework.websocket.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptorAdapter;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+import java.security.Principal;
+
+/**
+ * Created by XiuYin.Cui on 2018/5/2.
+ */
+
+@Configuration
+//使用STOMP协议来传输基于消息代理的消息
+@EnableWebSocketMessageBroker
+@PropertySource("classpath:resources.properties")
+public class WebSocketStompConfig extends AbstractWebSocketMessageBrokerConfigurer {
+
+    @Value("${rabbitmq.host}")
+    private String host;
+
+    @Value("${rabbitmq.port}")
+    private Integer port;
+
+    @Value("${rabbitmq.userName}")
+    private String userName;
+
+    @Value("${rabbitmq.password}")
+    private String password;
+
+    /**
+     * 将 "/stomp" 注册为一个 STOMP 端点。这个路径与之前发送和接收消息的目的地路径有所
+     * 不同。这是一个端点，客户端在订阅或发布消息到目的地路径前，要连接到该端点。
+     *
+     * @param registry
+     */
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        //添加STOMP协议的端点。这个HTTP URL是供WebSocket或SockJS客户端访问的地址
+        //指定端点使用SockJS协议
+        registry.addEndpoint("/stomp").withSockJS();
+    }
+
+
+    /**
+     * 如果不重载它的话，将会自动配置一个简单的内存消息代理，用它来处理以"/topic"为前缀的消息
+     *
+     * @param registry
+     */
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        //基于内存的STOMP消息代理
+        registry.enableSimpleBroker("/queue", "/topic");
+
+        //基于RabbitMQ 的STOMP消息代理
+        /* registry.enableStompBrokerRelay("/queue", "/topic")
+                .setRelayHost(host)
+                .setRelayPort(port)
+                .setClientLogin(userName)
+                .setClientPasscode(password);*/
+
+        registry.setApplicationDestinationPrefixes("/app", "/foo");
+        registry.setUserDestinationPrefix("/user");
+    }
+
+
+    /**
+     * 1、设置拦截器
+     * 2、首次连接的时候，获取其Header信息，利用Header里面的信息进行权限认证
+     * 3、通过认证的用户，使用 accessor.setUser(user); 方法，将登陆信息绑定在该 StompHeaderAccessor 上，在Controller方法上可以获取 StompHeaderAccessor 的相关信息
+     * @param registration
+     */
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new ChannelInterceptorAdapter() {
+            @Override
+            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+                StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+                //1、判断是否首次连接
+                if (StompCommand.CONNECT.equals(accessor.getCommand())){
+                    //2、判断用户名和密码
+                    String name = accessor.getNativeHeader("username").get(0);
+                    String pas = accessor.getNativeHeader("password").get(0);
+
+                    if ("admin".equals(name) && "admin".equals(pas)){
+                        Principal principal = () -> name;
+                        accessor.setUser(principal);
+                        return message;
+                    }else {
+                        return null;
+                    }
+                }
+                //不是首次连接，已经登陆成功
+                return message;
+            }
+        });
+    }
+}
